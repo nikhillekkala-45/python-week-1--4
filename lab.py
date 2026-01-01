60a43c5c0cfc175220b10595c801b301046269f8
nikhillekkala-45 <nikhillekkala2@gmail.com>
2026-01-02 02:54:56 +0530

    Add files via upload

---
+++ lab.py
@@ -0,0 +1,7 @@
+import math
+print("value of pi:",math.pi)
+num=25
+print("square root of ",num,"is:",math.sqrt(num))
+angle=math.pi/4
+print("cos(45 degree):",math.cos(angle))
+print("sin(45 degree):",math.sin(angle))
